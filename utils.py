49628bf00815ef5c7c0447a7022ec540812321f1
racklyn <racklynsotero@gmail.com>
2022-02-13 23:15:17 -0300

    Adding time and pause features

--- utils.py
+++ utils.py
@@ -59,6 +59,7 @@ def goal(goalP, p1, p2, screen):
 
     goalPlayer.score += 1
 
+    # Draw goal window
     goalBox = pygame.Surface((int(consts.SCREEN_WIDTH/2), int(consts.SCREEN_HEIGHT/2)))
     goalBox.set_alpha(230)
     goalBox.fill(consts.BLACK)
@@ -76,7 +77,8 @@ def goal(goalP, p1, p2, screen):
     text_rect = scoresText.get_rect(center=(goalBox.get_width()/2, goalBox.get_height()/2 + 20))
     goalBox.blit(scoresText, text_rect)
 
-    screen.blit(goalBox, (int(consts.SCREEN_WIDTH/4),int(consts.SCREEN_HEIGHT/4)))
+    goalBox_rect = goalBox.get_rect(center=(screen.get_width()/2, screen.get_height()/2))
+    screen.blit(goalBox, goalBox_rect)
 
     pygame.display.flip()
 
@@ -92,12 +94,85 @@ def setToInitialState(p1, p2, ball):
 
 
 
-def drawTopMenu(screen, p1, p2, font):
+def drawTopMenu(screen, p1, p2, timer, font):
     pygame.draw.rect(screen, consts.WHITE, 
         (0, 0, consts.SCREEN_WIDTH, consts.TOP_MENU_HEIGHT))
 
     p1ScoreText = font.render("P1: %d"%p1.score, False, p1.color)
-    p2coreScore = font.render("P2: %d"%p2.score, False, p2.color)
-    screen.blit(p1ScoreText, (consts.SCREEN_WIDTH-100, 10))
-    screen.blit(p2coreScore, (30, 10))
+    p1Score_rect = p1ScoreText.get_rect(center=(80, consts.TOP_MENU_HEIGHT/2))
 
+    p2coreScore = font.render("P2: %d"%p2.score, False, p2.color)
+    p2Score_rect = p2coreScore.get_rect(center=(consts.SCREEN_WIDTH - 80,  consts.TOP_MENU_HEIGHT/2))
+
+    timerText = font.render("%d:00 min."%timer, False, consts.GRAY)
+    timer_rect = timerText.get_rect(center=(consts.SCREEN_WIDTH/2,  consts.TOP_MENU_HEIGHT/2))
+
+    screen.blit(p1ScoreText, p1Score_rect)
+    screen.blit(p2coreScore, p2Score_rect)
+    screen.blit(timerText, timer_rect)
+
+
+def drawGamePausedMenu(screen, option, events):
+
+    pausedBox = pygame.Surface((int(consts.SCREEN_WIDTH/4), int(consts.SCREEN_HEIGHT/4)))
+    pausedBox.set_alpha(230)
+    pausedBox.fill(consts.WHITE)
+
+    font = pygame.font.SysFont(None, 30, True, False)
+    title = font.render("Jogo pausado!", False, consts.GRAY)
+    text_rect = title.get_rect(center=(pausedBox.get_width()/2, pausedBox.get_height()/2 - 30))
+    pausedBox.blit(title, text_rect)
+
+    font = pygame.font.SysFont(None, 20, True, False)
+    op1 = font.render("Retomar", False, consts.RED if option==0 else consts.GRAY)
+    text_rect = op1.get_rect(center=(pausedBox.get_width()/2, pausedBox.get_height()/2 + 20))
+    pausedBox.blit(op1, text_rect)
+
+    font = pygame.font.SysFont(None, 20, True, False)
+    op2 = font.render("Sair da partida", False, consts.RED if option==1 else consts.GRAY)
+    text_rect = op2.get_rect(center=(pausedBox.get_width()/2, pausedBox.get_height()/2 + 50))
+    pausedBox.blit(op2, text_rect)
+
+    pausedBox_rect = pausedBox.get_rect(center=(screen.get_width()/2, screen.get_height()/2))
+    screen.blit(pausedBox, pausedBox_rect)
+
+    pygame.display.flip()
+
+
+    
+
+def endGameWindow(p1, p2, screen):
+
+    endGameBox = pygame.Surface((int(consts.SCREEN_WIDTH/3), int(consts.SCREEN_HEIGHT/3)))
+    endGameBox.fill(consts.BLACK)
+
+    font = pygame.font.SysFont(None, 40, True, False)
+    title = font.render("FIM DE JOGO !", False, consts.WHITE)
+    text_rect = title.get_rect(center=(endGameBox.get_width()/2, endGameBox.get_height()/2 - 20))
+    endGameBox.blit(title, text_rect)
+
+    scoresText = font.render("%d x %d"%(p2.score, p1.score), False, consts.WHITE)
+    text_rect = scoresText.get_rect(center=(endGameBox.get_width()/2, endGameBox.get_height()/2 + 20))
+    endGameBox.blit(scoresText, text_rect)
+
+    font2 = pygame.font.SysFont('Arial', 14, False, False)
+    msg = font2.render("- Pressione SPACE para começar um novo jogo -", True, consts.LIGHT_GRAY)
+    text_rect = msg.get_rect(center=(endGameBox.get_width()/2, endGameBox.get_height() - 30))
+    endGameBox.blit(msg, text_rect)
+
+    endGameBox_rect = endGameBox.get_rect(center=(screen.get_width()/2, screen.get_height()/2))
+    screen.blit(endGameBox, endGameBox_rect)
+
+    pygame.display.flip()
+
+
+
+
+def updateGameTimer(timer, counter):
+    if counter >= consts.FPS:
+        timer += 1
+        counter = 0
+    else:
+        counter += 1
+    
+    return timer, counter
